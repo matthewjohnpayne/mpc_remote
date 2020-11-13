ab3a12be520c572aaa9456d0f11108aa14bbab46
matthewjohnpayne <matthewjohnpayne@gmail.com>
2020-11-13 14:02:40 -0500

    json-send + sample-data

--- sockets_class.py
+++ sockets_class.py
@@ -95,6 +95,7 @@ class Shared():
             raise error('You can only send pickleable data')
             
         # send the length of the serialized data first
+        print(f"_send: len(serialized)={len(serialized)} , struct.pack('>I', len(serialized))={struct.pack('>I', len(serialized))}")
         s.send(struct.pack('>I', len(serialized)))
                 
         # send the encoded serialized data

bd1fef4a378e364b650879c3aa39e3e33fd807b8
matthewjohnpayne <matthewjohnpayne@gmail.com>
2020-11-14 07:39:41 -0500

    json-send + sample-data

--- deploy.py
+++ deploy.py
@@ -19,16 +19,16 @@ import sockets_class as sc
 
 if len(sys.argv) == 2 and sys.argv[1] in ["W","M"]:
 
-  # This is for the web-server ...
+  # This is for the web-server (e.g. mpcweb1) ...
   # ... it needs to have some simple cgi script available ...
   if sys.argv[1] == "W" : 
     print("Deploying code on web-server: This is currently a primitive script-copy")
-    for script in ["orbfit.cgi"]:
+    for script in ["orbfit.cgi","sockets_class.py"]:
       command = "sudo cp %s /var/www/cgi-bin/cgipy" % script
       print("\t", command)
       os.system(command)
 
-  # This is for the compute cluster ...
+  # This is for the compute cluster (e.g. marsden)...
   # ... this needs to have a socket-server to receive incoming requests ...
   elif sys.argv[1] == "M":
     OS = sc.OrbfitServer()

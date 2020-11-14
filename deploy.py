e16302a486ad9f8b5966f6d018bd59e298fe8c21
matthewjohnpayne <matthewjohnpayne@gmail.com>
2020-11-14 09:22:26 -0500

    json-send + sample-data

--- deploy.py
+++ deploy.py
@@ -19,24 +19,30 @@ import sockets_class as sc
 
 if len(sys.argv) == 2 and sys.argv[1] in ["W","M"]:
 
-  # This is for the web-server (e.g. mpcweb1) ...
-  # ... it needs to have some simple cgi script available ...
-  if sys.argv[1] == "W" : 
-    print("Deploying code on web-server: This is currently a primitive script-copy")
-    for script in ["orbfit.cgi"]:
-      command = "sudo cp %s /var/www/cgi-bin/cgipy" % script
-      print("\t", command)
-      os.system(command)
+    # This is for the web-server (e.g. mpcweb1) ...
+    # ... it needs to have some simple cgi script available ...
+    # ... and also I can't get the python scripts to play nice unless I link them into the cgi-directory
+    if sys.argv[1] == "W" :
+        print("Deploying code on web-server: This is currently a primitive copy-to-directory script ...")
+        for script in ["orbfit.cgi"]:
+            command = "sudo cp %s /var/www/cgi-bin/cgipy" % script
+            print("\t", command)
+            os.system(command)
+        for script in ["sockets_class.py", "sample_data.py"]:
+            command = "sudo ln -s /home/mpayne/mpc_socket_server/%s /var/www/cgi-bin/cgipy/%s" % (script,script)
+            print("\t", command)
+            os.system(command)
+      
 
-  # This is for the compute cluster (e.g. marsden)...
-  # ... this needs to have a socket-server to receive incoming requests ...
-  elif sys.argv[1] == "M":
-    OS = sc.OrbfitServer()
-    print(f"Launched socket server: OS.host={OS.host}, OS.port={OS.port}")
-    OS._listen( startup_func = True )
+    # This is for the compute cluster (e.g. marsden)...
+    # ... this needs to have a socket-server to receive incoming requests ...
+    elif sys.argv[1] == "M":
+        OS = sc.OrbfitServer()
+        print(f"Launched socket server: OS.host={OS.host}, OS.port={OS.port}")
+        OS._listen( startup_func = True )
 
-  else:
-    print("should not be able to see this error")
+    else:
+        print("should not be able to see this error")
 
 else:
-  print("input args not recognized ...", sys.argv)
+    print("input args not recognized ...", sys.argv)
